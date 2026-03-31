0778b6e01dc71e80c35b44d4aabab2b92e83984b
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2026-03-31 23:18:58 +0530

    vite migration

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API,
+  apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: "react-portfolio-ujjwal.firebaseapp.com",
   projectId: "react-portfolio-ujjwal",
   storageBucket: "react-portfolio-ujjwal.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
